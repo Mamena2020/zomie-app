eb64386e80a7517865edb0269166ee3f6a6bee2d
mamena2020 <mamenaware751@gmail.com>
2022-12-02 19:59:49 +0900

    [1]. seting view-> Copyright

--- lib/Views/Setting/SettingView.dart
+++ lib/Views/Setting/SettingView.dart
@@ -263,27 +263,30 @@ class _SettingViewState extends State<SettingView> {
               width: size.width * 0.7,
               height: size.height * 0.7,
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text.rich(
-                  TextSpan(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(
                     children: [
-                      TextSpan(
-                          text:
-                              'MIT License \n\nCopyright (c) 2022 Andre C Aipassa '),
-                      TextSpan(
-                        text:
-                            '\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:',
+                      Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                                text:
+                                    'MIT License \n\nCopyright (c) 2022 Andre C Aipassa '),
+                            TextSpan(
+                              text:
+                                  '\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:',
+                            ),
+                            TextSpan(
+                                text:
+                                    '\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.'),
+                            TextSpan(
+                                text:
+                                    '\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE'),
+                          ],
+                        ),
                       ),
-                      TextSpan(
-                          text:
-                              '\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.'),
-                      TextSpan(
-                          text:
-                              '\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE'),
                     ],
-                  ),
-                ),
-              ));
+                  )));
         },
         child: ListTile(
           leading: Icon(Icons.info),
